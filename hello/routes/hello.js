178f3a85abeb468c43d5de72f382bcb3d9043b1b
Darien Tsai <smartdragondt@gmail.com>
2021-01-07 21:53:38 -0800

    lab done

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+  var name = req.params.userName;
+  if(name === undefined) {
+    name = 'World';
+  }
+  console.log('name');
   res.render('index', {
-  	'name': '',
+  	'name': name,
   });
 };
